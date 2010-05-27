da0ffa90ef18b1d2fa6fd5983fffb7ec1839d87e
ascruggs <aascruggs@gmail.com>
2010-05-27 08:38:37 -0500

    adding initial rails stack

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'ec6d29e7051845906714e6b56d11774ab74d20a40d0a2fb58dab0b3e96d36dfd724f407bdb62f8129c956cdf1abe4bbe6044f3d537e4278426bdeb164f725d25'
