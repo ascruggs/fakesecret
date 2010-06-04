960db84e7b28b253b8f255fdad26a44250a91b71
ascruggs <aascruggs@gmail.com>
2010-06-03 20:16:39 -0500

    this is disgusting, maybe heroku isn't the best choice

--- db/migrate/20100602225444_create_posts.rb
+++ db/migrate/20100602225444_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def self.up
     create_table :posts do |t|
-      t.string :week
+      t.date :week
 
       t.timestamps
     end
--- db/migrate/20100602232524_change_post_week_to_date.rb
+++
@@ -1,9 +0,0 @@
-class ChangePostWeekToDate < ActiveRecord::Migration
-  def self.up         
-    change_column :posts, :week, :date
-  end
-
-  def self.down                       
-    change_column :posts, :week, :string
-  end
-end
