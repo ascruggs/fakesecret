52d1a84044da248e0037c3631688055982cc082f
ascruggs <aascruggs@gmail.com>
2010-06-07 15:27:51 -0400

    "Uniqueness", its not just for the index.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base              
   has_many :secrets
+  validates_uniqueness_of :week
 end
--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,6 +1,7 @@
 class Secret < ActiveRecord::Base
   belongs_to  :post
-  has_many    :votes
+  has_many    :votes  
+  validates_uniqueness_of :image_url
   
   def vote_count(vote)
     self.votes.where(:opinion => Vote::VALUES[vote]).sum(:opinion).abs
